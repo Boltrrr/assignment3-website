b4addb48a38e821a2b5321b6e583daf405072f84
Boltr <132723335+Boltrrr@users.noreply.github.com>
2026-03-14 16:48:43 -0400

    Added back to top

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".nav-links");
 const navLinkItems = document.querySelectorAll(".nav-links a");
+const backToTop = document.getElementById("back-to-top");
 
+//Hamburger button for mobile
 if (navToggle && navLinks) {
   navToggle.addEventListener("click", function () {
     navLinks.classList.toggle("open");
@@ -12,4 +14,24 @@ if (navToggle && navLinks) {
       navLinks.classList.remove("open");
     });
   });
+}
+
+//Back to Top button
+if (backToTop) {
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 300) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+
+  backToTop.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+
 }
