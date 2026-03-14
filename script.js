0d2175865c281d40b5facdfb5abed6c18c9e454b
Boltr <132723335+Boltrrr@users.noreply.github.com>
2026-03-14 17:14:34 -0400

    Form validation

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 //Assignment 3 - IMD1005
 //Ryan Bolt - 2026
 
+
 //Constants
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".nav-links");
 const navLinkItems = document.querySelectorAll(".nav-links a");
 const backToTop = document.getElementById("back-to-top");
 const faqItems = document.querySelectorAll(".faq-item");
+const contactForm = document.getElementById("contact-form");
+
 
 //Hamburger button for mobile
 if (navToggle && navLinks) {
@@ -21,6 +24,7 @@ if (navToggle && navLinks) {
     });
   });
 }
+
 
 //Back to Top button
 if (backToTop) {
@@ -39,8 +43,8 @@ if (backToTop) {
       behavior: "smooth"
     });
   });
-
 }
+
 
 //Accordion FAQ
 faqItems.forEach(function (item) {
@@ -58,3 +62,50 @@ faqItems.forEach(function (item) {
     }
   });
 });
+
+
+//Form validation
+if (contactForm) {
+
+  contactForm.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
+
+    const errors = contactForm.querySelectorAll(".error-message");
+    const success = document.getElementById("form-success");
+
+    let valid = true;
+
+    errors.forEach(function(error){
+      error.textContent = "";
+    });
+
+    success.textContent = "";
+
+    if (name.value.trim() === "") {
+      errors[0].textContent = "Please enter your name.";
+      valid = false;
+    }
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailPattern.test(email.value)) {
+      errors[1].textContent = "Please enter a valid email address.";
+      valid = false;
+    }
+
+    if (message.value.trim().length < 10) {
+      errors[2].textContent = "Message must be at least 10 characters.";
+      valid = false;
+    }
+
+    if (valid) {
+      success.textContent = "Thank you, we have received your message.";
+      contactForm.reset();
+    }
+  });
+}
